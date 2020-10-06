b2d87abf280070fd629104f42691c96262581196
Jeferson <jeferson_jfs@hotmail.com>
2020-10-06 14:30:57 -0300

    fix bugs

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,16 +5,16 @@ import Board from './../Board/Board';
 import './Game.css';
 
 const Game = (players) => {
-    
-    const {player1, player2} = players;
+
+    const { player1, player2 } = players;
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [stepNumber, setStepNumber] = useState(0);
     const [xIsNext, setXisNext] = useState(true);
     const winner = calculateWinner(history[stepNumber]);
+    const [restart, setRestart] = useState(false);
+    const [exit, setExit] = useState(false);
     const xO = xIsNext ? 'X' : 'O';
-    const Pl = xIsNext ? player1 : player2;
-    console.log(player1, player2);
-
+    let Pl = xIsNext ? player1 : player2;
 
     const handleClick = (i) => {
         const historyPoint = history.slice(0, stepNumber + 1);
@@ -28,17 +28,25 @@ const Game = (players) => {
         setStepNumber(historyPoint.length);
         setXisNext(!xIsNext);
     };
-
+    const exitPage = () => {
+        window.location.reload(false);
+    }
     return (
-        <div className='game'>
-            
-            <Board squares={history[stepNumber]} onClick={handleClick} />
-            <div className='info-wrapper'>
+        <div>
+            <div className='game'>
 
-                <h3>{winner ? `Winner: ${winner}` : `'Next Player: ${Pl} ${xO}` }
-                </h3>
+                <Board squares={history[stepNumber]} onClick={handleClick} />
+                <div className='info-wrapper'>
+
+                    <h3>{winner ? `Winner: ${Pl = xIsNext ? player2 : player1} - ${winner}` : `'Next Player: ${Pl} - ${xO}`}
+                    </h3>
+                </div>
+
             </div>
-
+            <div className='buttons-game'>
+                <div className='btn-game' >Restart</div>
+                <div className='btn-game' onClick={exitPage}>Exit</div>
+            </div>
         </div>
     );
 };
