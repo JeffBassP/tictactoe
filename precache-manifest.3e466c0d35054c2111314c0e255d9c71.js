e71e4361906d7a0dfd239ef74b32cb298a840fbe
Jeferson <jeferson_jfs@hotmail.com>
2020-10-06 20:28:48 -0300

    Updates

---
+++ precache-manifest.3e466c0d35054c2111314c0e255d9c71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b175f865a5dfa2a0b4bf9531db5a6d47",
+    "url": "/JeffBassP/tictactoe/index.html"
+  },
+  {
+    "revision": "0c7b12432d7d6214fb68",
+    "url": "/JeffBassP/tictactoe/static/css/main.2e255deb.chunk.css"
+  },
+  {
+    "revision": "4ca90e0ee94885d936f2",
+    "url": "/JeffBassP/tictactoe/static/js/2.83b4832c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JeffBassP/tictactoe/static/js/2.83b4832c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7b12432d7d6214fb68",
+    "url": "/JeffBassP/tictactoe/static/js/main.d16f3328.chunk.js"
+  },
+  {
+    "revision": "d59533ffb24bfbe7be25",
+    "url": "/JeffBassP/tictactoe/static/js/runtime-main.131334cb.js"
+  }
+]);
